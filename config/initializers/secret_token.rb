483cf031db8b728fcdcd78ffe6b307cbc2bfcd8c
structuralartistry <dk@structuralartistry.com>
2013-04-11 12:51:41 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Structuralartistry3::Application.config.secret_token = '459179fdce12faf1dc20bec2bde0f6a2c3f4c3dfa5ebd99f190d20c814f7d7a30a3b8ff687df49ec3035434c01b609e23c4bf4f953c57a059f64479189b83b30'
